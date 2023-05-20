59749aa84efe1d34050a2b488c5d184c9549b22c
Angus Drake <angus.drake@flextrade.com>
2023-05-21 00:18:50 +0100

    adding game start control

---
+++ pre-game/game_control.cpp
@@ -0,0 +1,203 @@
+#include <string>
+#include <iostream>    
+#include <filesystem>
+#include <fstream>
+#include <sstream>
+
+// for sleep
+#include <chrono>
+#include <thread>
+
+// for repeating get user input calls
+#include <sys/select.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+namespace fs = std::filesystem;    
+using namespace std;    
+ 
+
+void printReadyUp()
+{
+
+    cout << R"(
+                                                                                                 :^.                                                                                                                                                                      
+                                                                              ~J?.              7&@G.                                                                                                                                                                     
+                                                                             :&@@?              J@@&:                                                                                                                                                                     
+                                                                 !55^        :&@@J              J@@&:                                                                                                                                                                     
+                                                                .B@@5        :&@@J              J@@&:                                                                                                                      .7?^                                           
+                                                                .B@@5        :&@@J     YB5.     J@@&:                                                                  !GG7                                                Y@@B.                                          
+                                                                .B@@P...:~7?YP@@@~     JGP.     J@@&:                                    :!?7!^                       :&@@Y                       .......                  5@@#.                   .^~:                   
+                                                                .B@@@&&&@@@@@@@@&:              J@@&:                                   J&@@@@@G:        .^!JYY!      ~@@@!        .~77~.      .?G&&&&&&&G.                5@@B.                 .^#@@@G!                 
+                                                                .B@@#PPPPY?~~B@@G.   .:.        J@@&::?Y7.                             ?@@@JY@@@~       ?#@@@@@@^     ~@@@!      :P&@@@@&~    :B@@&PPPPPPJ.                5@@B..:~JGG5:      :JG#&@&P@@@7                
+                                                                .B@@5       .#@@Y   ~&@G.       J@@&G&@@&^                            :&@@5 7@@@~      J@@@P7#@@5     ~@@@Y!!!JPYB@@#Y@@@7    5@@&^                        5@@&B&@@@@&G:     7&@@#B#Y^&@@?                
+                                                                .B@@5       ^&@@J   ?@@@!       J@@@@@GJ:                             ?@@@Y5&@@B:      P@@B  J@@@~    Y@@@@@@@@@@@@@?Y@@@^    G@@G                        :#@@@&B5J7^.      7@@@J    J@@@~                
+                                                                .#@@P       ?@@@^   :&@@Y       J@@@P^                                J@@@&@@GJ.       P@@B  .#@@G    !@@@Y!777~5@@@#@@&?     J@@&^                       ^&@@Y.            B@@#???YB@@@P                 
+                                                                 7GG!       ^B&G.   :#@@P       J@@&.                                 J@@@5!~:.:7PB5.  P@@B   J@@@^   ~@@@!     7@@@@#5:      7@@@~                       :&@@?             5@@@@@@@@BY!.                 
+                                                                              .     .B@@5       J@@&:                                 .5@@@@&&&@@@@B:  ^Y5~   :&@@P   ~@@@!      Y@@@Y^:.^YG5.7@@@~                       :&@@J             .^~~~~~~^                     
+                                                                                     .~^        7@@@!                                   ^J555555Y7:            P@@G   ~@@@!       ?#@@@@&@@@#:^#&B:                       :&@@J                                           
+                                                                                                 ^!^                                                           :??^   .JPY.        .^7YPPP57.  .:.                         ?P5:         .:.                               
+                                                                                                                                                                                                                                       :B@#^                              
+                                                                                                                                   75Y:                                                                                                !@@@!                              
+                                                                                                                                  :&@@J                                                                                                !@@@!                              
+                                                                                    ....                                          :&@@J                                                                              .!Y5PBBP?:        !@@@!                              
+                                                                                :?YG&&&&5                                         :&@@J                                                                           .JB&@@@@#&@@#?.      ~@@@7                              
+                                                                             .?B@@@@&GPP?    .75GGGGY^                            :&@@J                                                        ::   .?J~          ?@@@B?^:..J@@@5      .#@@5                              
+                                                                            ^#@@&P7:.       !&@@&#@@@G     !PPPP5~                .&@@J                                                      !B&@Y  5@@#.         ?@@@J    ^5@@@5       G@@P                              
+                                                                            ?@@@!        ^~!#@@B~^B@@B    7@@@@@@#Y5!         ^!!~7&@@J              :~:                                    .#@@B^ 7@@@Y          ^&@@G?JYG@@@B7.       5@@&^                             
+                                                                            ?@@&^       ~@@@@@@@&@@@&J   ^@@@G7P@@@@@^     .7G@@@@@@@@?     ^YY~    ~&@&^                                   ^@@@7 J@@@5           :&@@@@@@@#P!          !@@@^                             
+                                                                            7@@@~       .7Y&@@@BPYJ!.    .P@@@&&@@@@@Y    J&@@&P77&@@B.     P@@#.  .B@@@G:                                  ^@@@GB@@@Y            :&@@P!^^^.             ~7~                              
+                                                                            .#@@G.         ~B@@@#PYYY5Y?:  ~J5555?Y@@@Y. ?@@@5.  ?@@@7      ^&@@BJ?B@@@@@Y                                  .Y&&&&#G!             7@@@!                                                   
+                                                                             7@@@5           !JB&@@@@@@&!          J&@@G P@@@BGG#@@&?        ^5&@@@@@PG@@#:                                   .:::.               J@@&:                                                   
+                                                                              5##Y              .::::::.            :?J~ :YB#####G?.           .~!!!^ ~@@@!                                                       J@@&:                                                   
+                                                                                                                            ....                      ^&@@?                                                       J@@&:                                                   
+                                                                                                                                                      ^&@@?                                                       !&@B.                    .                              
+                                                                                                                                                      ^&@@?                                                        :^.                    Y&#^                            
+                                                                                                                                               .   .:!5@@@!                                                                               ~JJ:                            
+                                                                                                                                              5##GB#@@@@G7                                                                                                                
+                                                                                                                                              Y####BPJ?^                                                                                                                  
+\n)";
+}
+
+
+void moveCursorUp(int rows)
+{
+    for (int i; i < rows; i++)
+    {
+        cout << "\x1b[A";
+    }
+}
+
+
+void printStatus(bool skipClearLine = false)
+{
+    std::string path = ".players";
+    string file_name;
+    int  player_count = 0;
+    
+    bool allPass = true;
+
+    for (const auto & entry : fs::directory_iterator(path))
+    {
+        player_count++;
+        //file_name = entry.path();
+        //std::cout << entry.path() << std::endl;
+        //std::cout << file_name << std::endl;
+
+        std::ifstream file(entry.path());      
+
+        string ready_state, name;
+        if (file.is_open())    
+        {    
+            std::stringstream buffer;    
+            buffer << file.rdbuf();    
+            std::string fileContent = buffer.str();    
+            file.close();    
+            
+            if (!skipClearLine)
+            {
+                // clear current line - this should help with ghost letters 
+                printf("\33[2K\r");
+            }
+
+            int pos = fileContent.find(", ");
+            ready_state = fileContent.substr(0, pos);
+            name = fileContent.substr(pos + 2);
+            
+            if (ready_state == "true")
+            {
+                cout << "✅";
+            }
+            else
+            {
+                cout << "❌";
+                allPass = false;
+            }
+            cout << " " << name;    
+        }    
+        else    
+        {    
+            cout << "ERROR - cannot read file" << endl;    
+        }    
+    }
+    //printf("%c[%d;%df",0x1B,row,line_pos)
+    cout << "Player count: " << player_count <<endl;
+    moveCursorUp(player_count + 1); // plus 1 for the line above
+
+}
+
+bool getUserInput()    
+{    
+    std::string userInput;    
+    bool skip = false;    
+    
+    //std::cout << "Please enter your input within 5 seconds: " << endl;    
+    
+    fd_set readfds;    
+    FD_ZERO(&readfds);    
+    FD_SET(STDIN_FILENO, &readfds);    
+    
+    struct timeval timeout;    
+    timeout.tv_sec = 0.5;    
+    
+    timeout.tv_usec = 0;    
+    
+    int ready = select(STDIN_FILENO + 1, &readfds, NULL, NULL, &timeout);    
+    
+    if (ready == -1) {    
+        //std::cout << "Error in select." << std::endl;    
+        return false;    
+    getUserInput();    
+    }    
+    else if (ready == 0) {    
+        //std::cout << "Time limit exceeded. Skipping..." << std::endl;    
+        skip = true;    
+        return false;    
+    }    
+    else if (FD_ISSET(STDIN_FILENO, &readfds)) {    
+        std::getline(std::cin, userInput);    
+    }    
+    
+    if (!skip) {    
+        //std::cout << "User input: " << userInput << std::endl;    
+        // Further processing or actions based on user input    
+    }    
+return true;    
+}
+
+int main(int argc, char *argv[]){    
+
+    string player_name = argv[1];
+    string player_num = argv[2];
+
+
+   system("clear");     
+
+   printReadyUp();
+ 
+cout << endl;
+    moveCursorUp(35);
+
+    cout << player_name << " " << player_num << endl;
+
+    bool skipClearLine = true;
+    while (!getUserInput())
+    {
+        printStatus(skipClearLine);
+        std::this_thread::sleep_for(500ms);
+    }
+
+    cout << "Hit enter to ready up" << endl;
+    std::string input;    
+    std::getline(std::cin, input);    
+
+   system("clear");     
+
+    while (true)
+    {
+        printStatus();
+        std::this_thread::sleep_for(500ms);
+    }
+
+    return 0;  
+}
---
+++ pre-game/start_game.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+name="$@"
+
+[[ -z $name ]] && echo must give name as argument && exit
+
+countdown()
+{
+echo Game Starting in...    
+count=3    
+while [[ $count > 0 ]]; do    
+    echo $count    
+    ((count--))    
+    sleep 1    
+done 
+}
+
+
+clear
+
+printf '    
+
+                                              7GB?
+                                             .#@@J
+         :PBP7.                              .B@@7                                  :7!.                                                                                                                                                            ?G5:
+         :P#@@&J.                            ~@@@^                                  P@@?                                                                                                                                                           .#@@7
+            !B@@#7.                          ?@@B.                                  G@@J                                                                                                                                                           .#@@7
+              7B@@#!                        .B@@J                                   G@@J                                                                                                                                                           .#@@7
+                7&@@J       ^PGP7           !@@&:        :~?Y55555Y?!~:             G@@J                                                                   .~JYY?.                                                                                 .#@@7        .^^:      :!77777~:.
+                 ^B@@G~    ?&@@@@G^         G@@Y       ~G&@@@&&&&&&@@@@5            G@@J       ~7??!^                 .:::::           .~J55Y!.          ~P#@@@@@&J.       .~7?5BBBBBBBG55?^.                                                      .#@@7 .^~7J5P#@@@!  :?P&@@@@@@@@&#G5J?!.
+                  .J&@@Y. ^@@@7Y@@@P:      7@@&^      :&@@5~:. ....:J@@#.           G@@5    .?B@@@@@@7            !5PG&&@@@&BP!      ^Y#@@&&@@#?.     .7P@@@5!:~B@@B?   ~YB&@@@@#GGGGGG##@@@#P^                                                  :!?&@@#B&@@@@@##P7!  J&@@BY~^^^7JP#BB&@@@#J:
+                    ^B@@G^G@@P  ^P@@#?    ?@@@!       !@@&.  .YPPPPP#@@P            ?@@#.  ~#@@#?:7P5^          ~B@@&&@#YJJP&@@Y    ^#@@G!..7#@@#7   ~#@@BY^    .7#@@5.5@@@GJ7^^.     ..:~5@@@7                                                 :&@@@@@@GPJ!~^:..   ^B@@B~         .  .^7G@@#.
+                     :G@@@@@G.    7B@@P~ J@@&~        ~@@&!. :G#BBB#BP?.            :&@@! :B@@5                J@@&J..:.    5@@P   ~&@@5.    .7&@@J:J&@@J         :&@@#@@B~~B####&&&&&&##&@@&5:                                                  !?!^#@@7          .B@@Y                 !@@&:
+                       7#@@#^      .5&@@#@@#~          J&@@#7^.             :YGY.    B@@P 5@@#~..              G@@Y        .B@@5   5@@P        ~B@@&@@B!           7@@@@#: :Y5555555555555J!:                                                        P@@Y          .&@@?            ..:!J#@@B.
+                        .!7:         ^5#&#P:            .?B@@@&GJ7~~~~~~~!7P&@@#:    ~@@@77&@@@@&BBG55555GBP^  5@@&G55J????B@@P:   P@@5         :#@@@5.             P@@@#!.                                                                          5@@B.          J@@@&GP5J?????PB&&@@@&GJ^
+                                       ...                 ^7JG@@@@@@@@@@@@@#Y~.      ^7!. .^!JYPGG########P^  .75G##&@@@@@@#Y.    ~PP~          ^JY!               ~@@@@@B?^.                                                                       ^&@@7           :75G##&@@@@@@@B5YY?^.
+                                                               :^~!!!!!!!~^:.                       ......         ...::::^:.                                        ?PY7B@@@&5?~^^^^^^^^^^7J7.                                                       P@@B.              ...:::::^:
+                                                                                                                                                                          ^!5#@@@@@@@@@@@@@@@&~                                                       ^GBP:
+                                                                                                                                                                              :~7??????????7~.
+
+\n'
+
+printf '
+                .^?PB####BGJ:               ~?~    !GG7    :7?????????!.               .:~~!?????YG#&@@@&!.^~::::::^~~7??!.                                                                                                          .:::.               .^!?5G#@@@~
+              :5&@@@#GGGG&@@~              ^@@@~   G@@G    Y@@@@@@@@@@@!              P@@@@@@@@@@@&BG@@@@@@@@@&@@@@@@@@@@@7                   ^^.                                :~~~~~~~!7????J5PP5!  .^^                     .~?5B#&@@&&G!        ^7YGB&@@@@&BGP?.
+             :#@@@P?^.   .^.               ~@@@!   G@@G    .^^?@@@7^^^:               !YYY77#@@G^^.  JGPYYY#@@@5YYYY?7!^^:        JGP~      7B@@G                               ~@@@@@@@@@@@@@@@@&&&Y  P@@G!.     :JGB?      :Y#@@@&BG5YB@@&:      :&@@@#G5?!^.
+             .P&@@@@@&#BBPY?7~:            ~@@@!  .G@@G       !@@@~                         B@@5           ?@@&:                 .B@@&5~   ?@@@P^                               .?Y?77777J@@@J^^:...   ~P&@@&BY^:?&@@&?      J@@@B^.    Y@@&:      !@@@7
+               :~77?YPGB#&@@@@@#5!:        7@@@5JG&@@@G       !@@@~                        .B@@P           ?@@&:                  .7P@@@#5G@@&?                                          ^&@@7           :7YB@@@&@@&?.       ~@@@?  .^?G@@@Y       Y@@#.      .:~^:
+                         .:~!JG&@@&5:     .B@@@@@@&&@@G       !@@@~                        .B@@P           ?@@&:                     ~Y#@@@&Y:                                           ^&@@7               7&@@@J:          B@@G^Y#@@@#Y~       .B@@B~~~!?5B&@@@&^
+                               .7&@@P      !@@@?^^.G@@G       !@@@~                        .B@@P           ?@@&:                     :5&@@P:                                             ^&@@7             :J&@@G~            5@@@@@@G?^.         .#@@@@@@@@@#P5Y5J^
+               :5GPJ7~^^^^^^^^~~7#@@G      7@@@!   G@@G       !@@@~                        .B@@P           ?@@&:                   .?#@@B!                                               ^&@@7            7&@@#?              5@@@&5~             .#@@G77??JJY5B&@@@?
+               ~#@@@@@@@@@@@@@@@@@&B7      ?&&P.   G@@G       ~&@@5^!777777?Y!              B@@P           ?@@&:                  !#@@&J.                                                ^@@@7         ~JG@@@5.               7@@@!               .#@@BJ5B&@@@@&B5J7:
+                .:^~!7?????????7~^:         ..     J&&Y     Y&&@@@@@@@@@@@@@@B.            ^@@@5           ^GBY.                  ~GB5^                                                  :#@@!        .#@@@P^                 ~@@@!                5&&&@@#GY7^:.
+                                                    ..      7PP555J?7~~~~~~~~.             .JPJ:                                                                                          :~^          JBB7                   ~@@@?                 :::::.
+                                                                                                                                                                                                                              ~@@@5
+                                                                                                                                                                                                                              :B@#^
+                                                                                                                                                                                                                               .:.
+\n'
+
+
+player_directory=".players"
+[[ -d $player_directroy ]] && mkdir $player_directory
+
+if [[ -n "$(\ls -A $player_directory)" ]]; then
+    # file exists in the directory 
+    # increment from the last player number
+
+    last_file=$(\ls $player_directory/player_[0-9]* 2> /dev/null | tail -1)
+
+    ## BUG - ^ the ls command above doesn't order the files correctly so player_9 will always show as the last
+
+    file_name_regex='.*_([0-9]*)'
+    [[ $last_file =~ $file_name_regex ]] && num=${BASH_REMATCH[1]}
+
+    ((num++))
+
+else
+    num=1
+
+    read -p "Wait for other players? [y]/n: " input
+    
+    [[ $input == "n" || $input == "N" ]] && single_player=true
+
+    
+fi
+
+
+
+player_file="$player_directory/player_${num}"
+
+echo $name = player_$num
+
+#printf "unready\n$name\n" > $player_file
+printf "false, $name\n" > $player_file
+
+
+#read -p "Hit enter when you are ready play."
+
+#printf "true, $name\n" > $player_file
+
+
+
+./game_control.exe $name $num 
+
+while [ "$(cat game_control)" != true ]; do
+    sleep 0.1
+done    
+
+countdown
+
+
+
+echo GAME START
+
+echo GAME OVER
+
+#reset for next game
+rm -f $player_directory/player_*
